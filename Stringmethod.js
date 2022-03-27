b4cce0460b26dde380feb33bb912bb35789c2762
SeungBeomKim <kimsb7218@naver.com>
2022-03-27 18:08:45 +0900

    Update Stringmethod.js

--- Stringmethod.js
+++ Stringmethod.js
@@ -12,3 +12,26 @@ for(let i = 0;i<list.length;i++)
     newList.push(list[i].slice(4));
 }
 console.log(newList);
+
+function hasCola(str){
+    if(str.indexOf("콜라")>-1){
+         console.log("금칙어가 있습니다.");      
+    }
+    else{
+        console.log("통과");
+    }
+}
+hasCola("와 사이다가 짱이야!");
+hasCola("무슨소리, 콜라가 최고");
+hasCola("콜라");
+function hasCola2(str){
+    if(str.includes("콜라")){
+         console.log("금칙어가 있습니다.");      
+    }
+    else{
+        console.log("통과");
+    }
+}
+hasCola2("와 사이다가 짱이야!");
+hasCola2("무슨소리, 콜라가 최고");
+hasCola2("콜라");
